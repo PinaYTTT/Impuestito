9dce42333a31cbeaa19e74087b0313b3bac5b876
PinaYT <pinacomandos@gmail.com>
2023-01-02 14:18:58 -0300

    0.3Beta

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+readme = open("./README.md", "r")
+
+
+setup(
+    name='impuestito',
+    packages=['impuestito'],  # this must be the same as the name above
+    version='0.3Beta',
+    description='Impuestito es una API local para calcular impuestos al hacer compras al exterior,',
+    long_description=readme.read(),
+    long_description_content_type='text/markdown',
+    author='PinaYT',
+    author_email='contacto.pinayt@gmail.com',
+    # use the URL to the github repo
+    url='https://github.com/PinaYTTT/impuestito',
+    download_url='https://github.com/PinaYTTT/impuestito',
+    install_requires=['requests'],
+    keywords=['impuestos', 'argentina', 'dolar'],
+    classifiers=[ ],
+    license='MIT',
+    include_package_data=True
+)
